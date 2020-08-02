726d16414c391aa2182fbae558ef1b8ed7a3415d
bumis95 <bumis95@gmail.com>
2020-08-02 22:47:32 +0300

    Update ResultsViewModel.kt

--- app/src/main/java/com/rshack/rstracker/viewmodel/ResultsViewModel.kt
+++ app/src/main/java/com/rshack/rstracker/viewmodel/ResultsViewModel.kt
@@ -1,7 +1,27 @@
 package com.rshack.rstracker.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rshack.rstracker.model.data.Track
+import com.rshack.rstracker.model.repository.IRepository
+import com.rshack.rstracker.model.repository.RemoteRepository
+import kotlinx.coroutines.launch
 
 class ResultsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _tracks = MutableLiveData<List<Track>>()
+    val tracks: LiveData<List<Track>>
+        get() = _tracks
+
+    private val repository: IRepository = RemoteRepository()
+
+    init {
+        viewModelScope.launch {
+            // Load from firebase though repository
+            _tracks.value = repository.load()
+        }
+    }
+
 }
